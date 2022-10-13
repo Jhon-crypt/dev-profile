3d6d2acffe522c6b7ef02a058b1a908c2572c6c9
Jhon-crypt <johnoladele690@gmail.com>
2022-10-13 15:53:57 -0700

    Made some changes

--- app/views/index.php
+++ app/views/index.php
@@ -10,7 +10,45 @@ class indexView{
 
         $this->index_page = '
         
-        <b>Oladele John</a> © 2022
+        <!doctype html>
+
+        <html lang="en">
+
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="">
+            <meta name="author" content="Oladele John, JohnOx">
+            <title>Devprofile</title>
+
+            <!-- custom CSS -->
+            <link href="index.css" rel="stylesheet">
+
+            <!-- Bootstrap core CSS -->
+            <link href="./app/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+            <link rel="stylesheet" href="./app/assets/font-awesome/css/font-awesome.min.css">
+
+            <link rel="stylesheet" href="./app/assets/angular-loading-bar-master/angular-loading-bar-master/build/loading-bar.css" 
+            type="text/css" media="all">
+
+            <!-- JavaScript Bundle with Popper -->
+            <script src="./app/assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+            <!-- angular Js -->
+            <script src="./app/assets/angular-1.8.2/angular.min.js"></script>
+
+            <script src="./app/assets/angular-1.8.2/angular-route.min.js"></script>
+
+            <script src="./app/assets/angular-loading-bar-master/angular-loading-bar-master/build/loading-bar.min.js"></script>
+
+        </head>
+
+        <body ng-app="app" style="background-color:#010101;">
+
+        </body>
+
+        </html>
         
         ';
 
