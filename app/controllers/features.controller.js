b9bac927e6053404ba4e925e92a6bda98fd7f7f6
Jhon-crypt <johnoladele690@gmail.com>
2022-10-13 14:55:47 -0700

    Added the features controller

---
+++ app/controllers/features.controller.js
@@ -0,0 +1,27 @@
+app.controller("featuresCtrl", 
+
+    function($scope){
+
+        $scope.contents = [
+
+            {
+                title:'Highlight Projects',image_route:'app/images/feat.jpg'
+            },
+            {
+                title:'Showcase Skills',image_route:'app/images/features2.jpg'
+            },
+            {
+                title:'Share Experience',image_route:'app/images/features3.jpg'
+            },
+            {
+                title:'Diy Themes',image_route:'app/images/features4.jpg'
+            },
+            {
+                title:'Safe And Secure',image_route:'app/images/features5.jpg'
+            }
+
+        ]
+
+    }
+
+);
