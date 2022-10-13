8452da029bf3e8a8d44ce347465d791ad888675f
Jhon-crypt <johnoladele690@gmail.com>
2022-10-13 14:55:07 -0700

    Added the features layout

--- app/layouts/features.layout.js
+++ app/layouts/features.layout.js
@@ -1,59 +1,23 @@
 app.directive("features", function(){
 
     return {
-
+ 
         template : `
         
         <div class="container pb-5 mb-4 text-light" ng-controller="featuresCtrl">
 
             <ul class="nav justify-content-center">
 
-                <li class="nav-item">
+                <li class="nav-item" ng-repeat="x in contents">
 
                     <a class="nav-link text-light">
 
                         <div class="card bg-dark">
 
-                            <img class="card-img-top" src="app/images/feat.jpg" height=300" width="500" loading="lazy">
+                            <img class="card-img-top" src="{{ x.image_route }}" height=300" width="500" loading="lazy">
 
                             <div class="card-body text-light">
-                                <h3>Highlight Projects</h3>
-                            </div>
-
-                        </div>
-                    
-                    </a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link text-light">
-
-                        <div class="card bg-dark">
-
-                            <img class="card-img-top" src="app/images/features2.jpg" height=300" width="500" loading="lazy">
-
-                            <div class="card-body text-light">
-                                <h3>Showcase Skills</h3>
-                            </div>
-
-                        </div>
-                    
-                    </a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link text-light">
-
-                        <div class="card bg-dark">
-
-                            <img class="card-img-top" src="app/images/features3.jpg" height=300" width="500" loading="lazy">
-
-                            <div class="card-body text-light">
-                                <h3>Share Experience</h3>
+                                <h3>{{ x.title }}</h3>
                             </div>
 
                         </div>
