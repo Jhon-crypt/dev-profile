715eba94face74d7f53daf1956d4872ccca9edf7
Jhon-crypt <johnoladele690@gmail.com>
2022-10-13 14:57:06 -0700

    Added footer layout

---
+++ app/layouts/footer.layout.js
@@ -0,0 +1,83 @@
+app.directive("footer", function(){
+
+    return {
+
+        template : `
+        
+        <div class="container mb-4">
+
+            <ul class="nav justify-content-center">
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Login
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Signup
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Help
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Search devs
+                    </a>
+
+                </li>
+
+            </ul>
+
+            <ul class="nav justify-content-center">
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Twitter
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-light">
+                        Facebook
+                    </a>
+
+                </li>
+
+            </ul>
+
+            <ul class="nav justify-content-center">
+
+                <li class="nav-item">
+
+                    <a class="nav-link text-muted">
+                        <small>JohnOx © 2022</small>
+                    </a>
+
+                </li>
+
+            </ul>
+
+        </div>
+        
+        `
+
+    }
+
+});
