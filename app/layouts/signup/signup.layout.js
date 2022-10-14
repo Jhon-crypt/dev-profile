e801227bbc0fc0bfa7bb2b3ad6445e2c13755d56
Jhon-crypt <johnoladele690@gmail.com>
2022-10-13 17:05:28 -0700

    Made some chnages to the sign up layout

--- app/layouts/signup/signup.layout.js
+++ app/layouts/signup/signup.layout.js
@@ -4,17 +4,16 @@ app.directive("signup", function(){
 
         template : `
         
-        <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="container col-xxl-8 px-4 py-1">
 
-            <div class="row align-items-center g-lg-5 py-5">
-
-                <div class="col-lg-7 text-center text-lg-start">
-                    <img src="./app/images/image1.jpg" class="img-fluid" width="700" height="500" loading="lazy">
+            <div class="row flex-lg-row-reverse align-items-center g-3 py-3">
+                <div class="col-10 col-sm-4 col-lg-6">
+                    <img src="./app/images/image1.jpg" class="d-block mx-sm-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
                 </div>
 
-                <div class="col-md-10 mx-auto col-lg-5">
+                <div class="col-lg-6">
 
-                    <form class="p-4 p-md-5 border rounded-3 bg-light">
+                    <form class="p-4 p-md-5 border rounded-3 bg-dark">
 
                         <div class="form-floating mb-3">
                             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -27,9 +26,8 @@ app.directive("signup", function(){
                         </div>
 
                         <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
-                        <hr class="my-4">
-                        <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
                     </form>
+
                 </div>
 
             </div>
